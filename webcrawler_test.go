cc99d4795996fee4a26d006009c9748be8b740d0
redscaresu <redscaresu@users.noreply.github.com>
2022-03-20 16:53:06 +0000

    basic test to test a single function

--- webcrawler_test.go
+++ webcrawler_test.go
@@ -2,9 +2,26 @@ package webcrawler_test
 
 import (
 	"fmt"
+	"io/ioutil"
 	"testing"
+	"webcrawler"
+
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestWebcrawler(t *testing.T) {
-	fmt.Println("it works ;)")
+func TestCrawl(t *testing.T) {
+	got, err := webcrawler.Crawl("https://www.example.com/")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	want, err := ioutil.ReadFile("testdata/webcrawler.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+
 }
